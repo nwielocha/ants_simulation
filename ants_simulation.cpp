f9ba0b193324a1ad16fda3e850de90b1a446974e
Nicolas Wielocha <nicolaswielocha@macbook-pro-2.local>
2021-05-26 22:06:49 +0200

    initial commit

--- ants_simulation.cpp
+++ ants_simulation.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 
-// mapa skladajaca sie z gniazda 
+// mapa skladajaca sie z gniazda, pozywienia i przeszkod 
 // mrowka
 // feromon
 // poruszanie sie 
 // jedzenie
 
 int main() {
-    
+
+     
     return 0;
 }
