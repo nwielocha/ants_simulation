7c978cce2d64337d8e6be9922879e743e33f6fca
Nicolas Wielocha <nicolaswielocha@macbook-pro-2.local>
2021-05-27 21:55:07 +0200

    initial commit

--- ants_simulation.cpp
+++ ants_simulation.cpp
@@ -1,13 +1,33 @@
 #include <iostream>
 
-// mapa skladajaca sie z gniazda, pozywienia i przeszkod 
-// mrowka
+// Mrowka sklada sie z ciala, oraz ma swoje wspolrzedne na mapie 
+struct TMrowka 
+{
+    char cialo = '.';
+    int x, y;
+};
+
 // feromon
-// poruszanie sie 
+
+// Losowe poruszanie sie w lewo, prawo i prosto,
+// gdy mrowka natrafi na sciane idzie w inna strone 
+// (lub przechodzi przez nia)
+
 // jedzenie
 
 int main() {
+    // Mapa
+    char mapa[50][50];
 
-     
+    int i, j;
+    for (i = 0; i < 50; i++)
+    {
+        for (j = 0; j < 50; j++)
+        {
+            mapa[i][j] = 'x';
+        }
+            std::cout << mapa[i][j] << std::endl;
+    }
+    
     return 0;
 }
